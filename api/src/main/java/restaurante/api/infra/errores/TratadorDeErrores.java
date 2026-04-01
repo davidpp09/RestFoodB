50b8e9dab481662602bc9904533341b3741b61d7
davidpp09 <david9fran@gmail.com>
2026-04-01 09:55:50 -0600

    Tratamiento de errores

---
+++ api/src/main/java/restaurante/api/infra/errores/TratadorDeErrores.java
@@ -0,0 +1,31 @@
+package restaurante.api.infra.errores;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
+        var errores = e.getFieldErrors().stream()
+                .map(DatosErrorValidacion::new)
+                .toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarError500(Exception e) {
+        return ResponseEntity.status(500).body("Error interno en el servidor: " + e.getLocalizedMessage());
+    }
+
+
+}
